06eba352c360d7179aa6419ee6d51370ea72b64d
Brilyan <49208216+brilyyy@users.noreply.github.com>
2022-07-14 02:45:29 +0700

    update page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 5s linear infinite",
+      },
+    },
     fontFamily: {
       mono: ["Space Mono", "monospace"],
     },
