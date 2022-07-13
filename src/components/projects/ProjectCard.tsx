e691c2dffe3ea85fa4d750d07f30cee2c01e04bb
Brilyan <49208216+brilyyy@users.noreply.github.com>
2022-07-14 03:36:18 +0700

    update recent projects

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,43 +1,45 @@
-const ProjectCard = () => {
+interface ProjectCardProps {
+  name: string;
+  technologies: Array<string>;
+  image: string;
+  description: string;
+  url: string;
+}
+
+const ProjectCard = ({
+  name,
+  technologies,
+  image,
+  description,
+  url,
+}: ProjectCardProps) => {
   return (
-    <div className="flex flex-col gap-6">
-      <div className="flex flex-col items-center gap-x-8 rounded-md bg-slate-800 p-3 md:flex-row">
-        <div className="shrink-0">
-          <a href="/demo/astro-boilerplate">
-            <img
-              className="h-36 w-36 hover:translate-y-1"
-              src="assets/images/project-web-design.png"
-              alt="Project Web Design"
-              loading="lazy"
-            />
+    <div className="flex flex-col items-center gap-x-8 rounded-md bg-slate-700 p-3 md:flex-row">
+      <div className="shrink-0">
+        <img
+          className="h-36 w-36 hover:translate-y-1 rounded-md "
+          src={image}
+          alt={name}
+          loading="lazy"
+        />
+      </div>
+      <div>
+        <div className="flex flex-col items-center gap-y-2 md:flex-row">
+          <a className="hover:text-cyan-400" href={url} target="_blank">
+            <div className="text-xl font-semibold">{name}</div>
           </a>
-        </div>
-        <div>
-          <div className="flex flex-col items-center gap-y-2 md:flex-row">
-            <a className="hover:text-cyan-400" href="/demo/astro-boilerplate">
-              <div className="text-xl font-semibold">Project 1</div>
-            </a>
-            <div className="ml-3 flex gap-2">
-              <div className="rounded-md px-2 py-1 text-xs font-semibold bg-fuchsia-400 text-fuchsia-900">
-                Astro.js
+          <div className="ml-3 flex gap-2">
+            {technologies.map((technology, index) => (
+              <div
+                key={index}
+                className={`rounded-md px-2 py-1 text-xs font-semibold bg-blue-300 text-neutral-700`}
+              >
+                {technology}
               </div>
-              <div className="rounded-md px-2 py-1 text-xs font-semibold bg-lime-400 text-lime-900">
-                Web design
-              </div>
-              <div className="rounded-md px-2 py-1 text-xs font-semibold bg-sky-400 text-sky-900">
-                Tailwind.css
-              </div>
-              <div className="rounded-md px-2 py-1 text-xs font-semibold bg-rose-400 text-rose-900">
-                TypeScript
-              </div>
-            </div>
+            ))}
           </div>
-          <p className="mt-3 text-gray-400">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            bibendum. Nunc non posuere consectetur, justo erat semper enim, non
-            hendrerit dui odio id enim.
-          </p>
         </div>
+        <p className="mt-3 text-gray-400">{description}</p>
       </div>
     </div>
   );
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -2,17 +2,62 @@ import ProjectCard from "./ProjectCard";
 
 interface Project {
   name: string;
-  technologies: string[];
+  technologies: Array<string>;
   image: string;
   description: string;
+  url: string;
 }
 
-const data: Array<Project> = await fetch(
-  "https://brilyyy.github.io/data/projects.json"
-).then((res) => res.json());
+const data: Array<Project> = [
+  {
+    name: "Wiyata Academy",
+    technologies: ["react", "typescript", "bootstrap", "react-query"],
+    image: "https://wiyata.id/img/logo.svg",
+    url: "https://wiyata.id",
+    description:
+      "a online learning platform for everyone. learn from the best teachers in the world.",
+  },
+  {
+    name: "Greenmetric UNS",
+    technologies: [
+      "react",
+      "typescript",
+      "bootstrap",
+      "laravel",
+      "inertia-js",
+      "php",
+    ],
+    image: "https://uns.ac.id/id/wp-content/uploads/Logo-UNS-New-04.png",
+    url: "https://greenmetric.uns.ac.id/",
+    description:
+      "an application for surveying the environment of UNS. it is a web application that can be used by the civital staff.",
+  },
+  {
+    name: "Smart Information UNS",
+    technologies: ["laravel", "blade", "php", "mysql", "bootstrap"],
+    image: "https://uns.ac.id/id/wp-content/uploads/Logo-UNS-New-04.png",
+    url: "https://smartin.uns.ac.id/",
+    description:
+      "an application for collecting all university data in number. it is a web application that can be used by the civital staff.",
+  },
+];
 
 const Projects = () => {
-  return data.map((project, index) => <ProjectCard key={index} />);
+  return (
+    <div className="flex flex-col gap-6">
+      {data.map((project, i) => (
+        <ProjectCard
+          // @ts-ignore
+          key={i}
+          name={project.name}
+          technologies={project.technologies}
+          image={project.image}
+          description={project.description}
+          url={project.url}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
