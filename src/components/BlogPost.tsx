15cfbb2bec571007e5e196347482f9dc89e47480
Brilyan <49208216+brilyyy@users.noreply.github.com>
2022-07-14 17:36:31 +0700

    Update BlogPost.tsx

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -10,14 +10,14 @@ const BlogPost = ({ post }: any) => {
       <div className="overflow-hidden rounded-md bg-zinc-600">
         <div className="aspect-w-3 aspect-h-2">
           <img
-            className="h-full w-full object-cover object-center"
+            className=" h-52 w-full object-cover object-center"
             src={post.image_url}
             alt="Image post"
             loading="lazy"
           />
         </div>
         <div className="px-3 pt-4 pb-6 text-center">
-          <h2 className="text-xl font-semibold">{post.title}</h2>
+          <h2 className="text-xl font-semibold truncate">{post.title}</h2>
           <div className="mt-1 text-xs text-gray-400">Feb 6, 2020</div>
           <div className="mt-2 text-sm">
             {markdownToTxt(post.content).slice(0, 40)}...
