8b4d8d4462e16ab21aa8e3319a1eec897700116a
Brilyan <49208216+brilyyy@users.noreply.github.com>
2022-08-14 04:37:27 +0700

    add image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), image()],
   site: "https://brilyyy.github.io",
   experimental: {
-    integrations: true,
+    integrations: true
   },
   markdown: {
-    drafts: true,
-  },
+    drafts: true
+  }
 });
